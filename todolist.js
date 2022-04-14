549d965d034353312fe6703d508d9e00ded8dcc2
junyoung <no2jfamily@tukorea.ac.kr>
2022-04-14 20:25:14 +0900

    4/14 commit

--- todolist.js
+++ todolist.js
@@ -16,6 +16,24 @@ button.addEventListener('click', function() {
     addlist(li);
 });
 
+function addlist(li) {
+    const newLi = document.createElement('li');
+    newLi.className = 'todo-item';
+    newLi.innerHTML = `<div class="checkbox"></div> 
+    <div class="todo">${li}</div> <button class="delBtn">삭제</button>  <button class="change">수정</button>  <button class="complete">완료</button>` 
+    todo.appendChild(newLi);
+    insert.value = ''; //값을 추가하면 입력칸에는 값이 안뜨게 할거야
+    const shows = todo.querySelectorAll('.delBtn'); //입력한 값에 대해서도 삭제 할수 있도록 했다!
+    shows.forEach(show => {
+        show.addEventListener('click', (e) => {
+            let remove = e.target.parentNode;
+            let parent = remove.parentNode;
+            parent.removeChild(remove);
+        })
+    })
+}
+
+
 //하나만 삭제 로직
 /*
     현재 클릭한 버튼의 부모 html태그를 remove에 할당(즉 li)
@@ -29,12 +47,5 @@ delbutton.forEach(del => {
         parent.removeChild(remove);
     });
 });
-
-function addlist(li) {
-    const newLi = document.createElement('li');
-    newLi.innerHTML = li;
-    newLi.setAttribute("class", ".todo-item");
-    todo.appendChild(newLi);
-}
 
 
